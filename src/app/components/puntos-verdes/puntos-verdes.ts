95dea907764527feae0250ef02c35deb3ea944a2
danielcastillopalma <d.castillo3@duocuc.cl>
2025-07-03 17:54:52 -0400

    Mejoras visuales

--- src/app/components/puntos-verdes/puntos-verdes.ts
+++ src/app/components/puntos-verdes/puntos-verdes.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Apirest } from '../../services/apirest/apirest';
 import { CommonModule } from '@angular/common';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-puntos-verdes',
@@ -10,9 +12,18 @@ import { CommonModule } from '@angular/common';
 })
 export class PuntosVerdes {
   puntosVerdes: any;
-  constructor(private api: Apirest) {
+  constructor(private api: Apirest, private sanitizer: DomSanitizer) {
     this.api.getPuntosVerdes().then(resp => {
       this.puntosVerdes = resp;
     })
   }
+  getQrUrl(punto: any): string {
+    const url = 'https://' + punto;
+    return 'https://api.qrserver.com/v1/create-qr-code/?data=' + encodeURIComponent(url) + '&size=100x100';
+  }
+  getMapaUrl(lat: number, lng: number): SafeResourceUrl {
+    const url = `https://www.google.com/maps?q=${lat},${lng}&z=15&output=embed`;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+
 }
